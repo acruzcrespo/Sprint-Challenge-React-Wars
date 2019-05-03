0742089472a49200a3e9a2d8e4a1d577594627d0
Ash <ashley.cruz.crespo@gmail.com>
2019-05-03 11:58:55 -0400

    working on Appjs

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
+import CharacterCard from './components/CharacterCard';
+import './components/StarWars.css';
+
 
 class App extends Component {
   constructor() {
@@ -33,6 +36,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="Header">React Wars</h1>
+        <div className="main-content-container">
+        
+        <CharacterCard starwarsChars={this.state.starwarsChars} />
+        </div>
       </div>
     );
   }
